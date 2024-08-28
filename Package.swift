a14717d23c6023f66dafd3aabd4f8cd0f4a1f6fc
kivyswiftlink <kivyswiftlink@gmail.com>
2024-08-28 20:51:04 +0000

    Update from https://github.com/kivyswiftlink/CoreBuilder/commit/4952d3e450a47fed2ec7581bdb806654d542fe50

---
+++ Package.swift
@@ -0,0 +1,139 @@
+
+// swift-tools-version: 5.8
+
+import PackageDescription
+
+let package = Package(
+    name: "KivyExtra", 
+    platforms: [.iOS(.v13)], 
+    products: [
+    	.library(
+        	name: "KiwiSolver", 
+        	targets: [
+        		"KiwiSolver"
+        	]
+    	), 
+    	.library(
+        	name: "FFMpeg", 
+        	targets: [
+        		"FFMpeg"
+        	]
+    	), 
+    	.library(
+        	name: "FFPyplayer", 
+        	targets: [
+        		"FFPyplayer"
+        	]
+    	), 
+    	.library(
+        	name: "MatPlotLib", 
+        	targets: [
+        		"MatPlotLib"
+        	]
+    	), 
+    	.library(
+        	name: "MaterialYouColor", 
+        	targets: [
+        		"MaterialYouColor"
+        	]
+    	), 
+    	.library(
+        	name: "Pillow", 
+        	targets: [
+        		"Pillow"
+        	]
+    	), 
+    ], 
+    dependencies: [
+    	.package(url: "https://github.com/KivySwiftLink/PythonCore", .upToNextMajor(from: "311.0.0"))
+    ], 
+    targets: [
+    	.target(
+        	name: "KiwiSolver", 
+        	dependencies: [
+        		.product(name: "PythonCore", package: "PythonCore"), 
+        		"libkiwisolver", 
+        	], 
+        	resources: [
+        	], 
+        	linkerSettings: [
+        	]
+    	), 
+    	.target(
+        	name: "FFMpeg", 
+        	dependencies: [
+        		.product(name: "PythonCore", package: "PythonCore"), 
+        		"libavdevice", 
+        		"libavfilter", 
+        		"libswresample", 
+        		"libavformat", 
+        		"libswscale", 
+        		"libavresample", 
+        		"libavutil", 
+        		"libavcodec", 
+        	], 
+        	resources: [
+        	], 
+        	linkerSettings: [
+        		.linkedFramework("VideoToolbox"), 
+        	]
+    	), 
+    	.target(
+        	name: "FFPyplayer", 
+        	dependencies: [
+        		.product(name: "PythonCore", package: "PythonCore"), 
+        		.product(name: "FFMpeg", package: "FFMpeg"), 
+        		"libffpyplayer", 
+        	], 
+        	resources: [
+        	], 
+        	linkerSettings: [
+        		.linkedFramework("CoreVideo"), 
+        		.linkedFramework("CoreMedia"), 
+        		.linkedFramework("CoreImage"), 
+        		.linkedFramework("AVFoundation"), 
+        		.linkedFramework("UIKit"), 
+        		.linkedFramework("CoreMotion"), 
+        	]
+    	), 
+    	.target(
+        	name: "MatPlotLib", 
+        	dependencies: [
+        		.product(name: "PythonCore", package: "PythonCore"), 
+        		.product(name: "KiwiSolver", package: "KiwiSolver"), 
+        		"libmatplotlib", 
+        	], 
+        	resources: [
+        	], 
+        	linkerSettings: [
+        	]
+    	), 
+    	.target(
+        	name: "Pillow", 
+        	dependencies: [
+        		.product(name: "PythonCore", package: "PythonCore"), 
+        		"libfreetype", 
+        		"libpillow", 
+        		"libjpeg", 
+        	], 
+        	resources: [
+        	], 
+        	linkerSettings: [
+        	]
+    	), 
+    	.binaryTarget(name: "libkiwisolver", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libkiwisolver.zip", checksum: "eb7c984114c6fffbd7c63ae609027f05a420d3fd35bd1da2364b5c4ea1238e2b"), 
+    	.binaryTarget(name: "libavdevice", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libavdevice.zip", checksum: "21ea7d8d43e3d81205180ca5797967fb81250a73a6e5cdfc9165931c0733bdfd"), 
+    	.binaryTarget(name: "libavfilter", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libavfilter.zip", checksum: "d7a79a1367bc27555228c90828db2263417b42d97fb6fbd4bbf6213f5f4950e7"), 
+    	.binaryTarget(name: "libswresample", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libswresample.zip", checksum: "0ead91d28a5d470605032556e71912a6c2da31dbe7197b7dc02f4680111bc643"), 
+    	.binaryTarget(name: "libavformat", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libavformat.zip", checksum: "2f66783cc55a5eb3c82c8f5cbfb4a7e4e5d7f742b7f00092bae369ecb51dc781"), 
+    	.binaryTarget(name: "libswscale", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libswscale.zip", checksum: "1f8dd531f5795406f69986cbe6de3a3f136b1d5f407fde2cf11b3566b65fa97c"), 
+    	.binaryTarget(name: "libavresample", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libavresample.zip", checksum: "1a6ff5b85d54455582a9f37a8f964c03dc9440bc3e903725186d0fd5d3336ca2"), 
+    	.binaryTarget(name: "libavutil", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libavutil.zip", checksum: "874a76c469c46d718708d7a8c617833cb1241fdda310bf63a637834621a66686"), 
+    	.binaryTarget(name: "libavcodec", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libavcodec.zip", checksum: "2bf8e9ecdf2faf985bda1d41a94578e4ce71816d3d7842509afbd80ac7c2dce3"), 
+    	.binaryTarget(name: "libffpyplayer", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libffpyplayer.zip", checksum: "ca1a9119b219b5391c2ebe09875e1876799c08472b9d6110add35e509b81c228"), 
+    	.binaryTarget(name: "libmatplotlib", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libmatplotlib.zip", checksum: "47be4f2bddfe064bc69fc18f0c3ec07ffeb9236e49a7e83471f3e358bec3b95e"), 
+    	.binaryTarget(name: "libfreetype", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libfreetype.zip", checksum: "2c1b10c3722ea9ba75a269ccc86ce596c3db74c5efe0370c40a27ca1df2b8cbc"), 
+    	.binaryTarget(name: "libpillow", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libpillow.zip", checksum: "e91e2fde3a2718a01cabf72680f9e2733936d46e8619bdace8839a0fcd65a875"), 
+    	.binaryTarget(name: "libjpeg", url: "https://github.com/KivySwiftLink/KivyExtra/releases/download/311.0.0/libjpeg.zip", checksum: "9dccd2615ef6b91a61279ae77be33ba9d378ef9b806b79348bc1eea94e9b705b"), 
+    ]
+)
--- README.md
+++
@@ -1,1 +0,0 @@
-# KivyExtra
